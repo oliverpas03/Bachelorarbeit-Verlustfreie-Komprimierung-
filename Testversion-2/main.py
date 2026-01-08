b9cfd3bc8a16b96c25c2ff7586ed21007a133613
oliverpas03 <oliverpas03@zedat.fu-berlin.de>
2026-01-08 18:39:49 +0100

    Häufigkeitstabelle als Array

--- Testversion-2/main.py
+++ Testversion-2/main.py
@@ -8,11 +8,33 @@ import decoder as dec
 import encoder as enc 
 
 
-data_filename = 'Data/LOG10201_Jährlinge.txt'
+data_filename = 'Data/LOG10202.txt'
 
+
+    
+    
+"""_summary_
+
+    Args:
+        originaldaten (_type_): Eine numpy array Zeile der originalen Daten
+        codierte_daten (string): Zeile in kodierter Form als String
+
+    Returns:
+        _type_: _description_
+""" 
 def compression_ratio(originaldaten , codierte_daten):
+   
    return (len(originaldaten)* 16)/ len(codierte_daten)
 def remove_uneccesary_data( data) -> NDArray[np.int16]:
+    """_summary_: Bereinigung der Daten und Umwandlung in numpy Array
+                  
+
+    Args:
+        data (_type_): Originaldaten als pandas Datenrahmen
+
+    Returns:
+        NDArray[np.int16]: bereinigte Daten als numpy Array
+    """
     data = data[data.iloc[:, 0] != 'GPS']
     data = data.iloc[:, 5:]
     data = data.reset_index(drop=True)
@@ -23,7 +45,6 @@ def remove_uneccesary_data( data) -> NDArray[np.int16]:
 
     return array_int
 
-
 if __name__ == "__main__":
   
  
@@ -32,7 +53,7 @@ if __name__ == "__main__":
     
     # Erstelle Test-Datei
     with open('test_sensor.csv', 'w', encoding='utf-8') as f:
-        f.write("""ACC,3890794474,10202,18.04.2023,08:14:34,2235,-756,-7989,2261,-550,-8678,2275,-110,-8551,2012,-348,-8211,296,767,-7622,-1774,2124,-6800,-2526,1343,-5542,-2067,845,-5936,-1842,83,-5208,-4476,-128,-7001,-3784,1004,-7966,-3819,-359,-7739,-23352,4377,-15542,16702,-20274,-28443,-807,2352,-3144,1028,-78,-8086,4109,-2412,-7921,3506,-249,-8096,2867,-616,-7887,2027,-280,-8112
+        f.write("""ACC,3890794474,10202,18.04.2023,08:14:34,-32768,32767,0,2261,-550,-8678,2275,-110,-8551,2012,-348,-8211,296,767,-7622,-1774,2124,-6800,-2526,1343,-5542,-2067,845,-5936,-1842,83,-5208,-4476,-128,-7001,-3784,1004,-7966,-3819,-359,-7739,-23352,4377,-15542,16702,-20274,-28443,-807,2352,-3144,1028,-78,-8086,4109,-2412,-7921,3506,-249,-8096,2867,-616,-7887,2027,-280,-8112
                 ACC,3890795013,10202,18.04.2023,08:23:33,2808,-7388,-1769,2351,-7460,-1613,2275,-7522,-2104,2981,-7631,-1657,3827,-6816,-2580,3901,-7795,-2559,4284,-6823,-2753,3817,-7296,-2035,4299,-6793,-1614,4057,-6686,-1798,4085,-6946,-1752,4617,-6457,-1799,4582,-6648,-1353,4316,-6606,-1646,4722,-6464,-1587,4285,-6777,-2738,4848,-6532,-2009,4223,-6929,-2158,3924,-6693,-2354,4549,-6504,-1602""")
 
     
@@ -54,8 +75,11 @@ if __name__ == "__main__":
     data_array = remove_uneccesary_data(data)
     original_daten = data_array.copy()
 
+
+
     differences = enc.calculate_differences(data_array)
-    frequencys = huff.determine_frequency(data_array)
+    frequencys = huff.calculate_frequencys(differences)
+   
 
     huffman_tree = huff.generate_huffmantree( frequencys)
 
